b1b991e0647bc4f5dc9368cfee958443d3e5c02b
MellowCo <799478052@qq.com>
2022-08-08 20:40:05 +0800

    refactor: update transformInclude

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { createUnplugin } from 'unplugin'
+import { createFilter } from '@rollup/pluginutils'
 import type { Options } from './types'
 import { extractorAttributify } from './core/extractor'
+
+const filter = createFilter(
+  [/\.[jt]sx?$/, /\.vue$/, /\.vue\?vue/],
+  [/[\\/]node_modules[\\/]/, /[\\/]\.git[\\/]/],
+)
 
 export default createUnplugin<Options>((options) => {
   const extractor = extractorAttributify(options)
@@ -9,7 +15,7 @@ export default createUnplugin<Options>((options) => {
     name: 'unplugin-unocss-attributify-wechat',
     enforce: 'pre',
     transformInclude(id) {
-      return !id.includes('node_modules') && /.vue$/.test(id)
+      return filter(id)
     },
     transform(code) {
       return extractor(code)
