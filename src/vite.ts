8cac0ee0142d4c96831723a10df07718372ce731
MellowCo <799478052@qq.com>
2022-08-08 13:05:06 +0800

    chore: export default

--- src/vite.ts
+++ src/vite.ts
@@ -2,9 +2,8 @@ import type { Options } from './types'
 import { defaultAttributes } from './core/extractor'
 import unplugin from '.'
 
-export {
+// TODO: some upstream lib failed generate invalid dts, remove the any in the future
+export default {
+  presetAttributifyWechat: unplugin.vite as (options?: Options) => any,
   defaultAttributes,
 }
-
-// TODO: some upstream lib failed generate invalid dts, remove the any in the future
-export default unplugin.vite as (options?: Options) => any
