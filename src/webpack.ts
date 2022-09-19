64ee1194edcad1f1d52c6bac368129819aacd382
MellowCo <799478052@qq.com>
2022-09-19 19:18:53 +0800

    refactor: presetAttributifyWechat rename to attributifyToClass

--- src/webpack.ts
+++ src/webpack.ts
@@ -2,11 +2,11 @@ import type { Options } from './types'
 import { defaultAttributes, defaultIgnoreNonValuedAttributes } from './core'
 import unplugin from '.'
 
-const presetAttributifyWechat = unplugin.webpack as (options?: Options) => any
+const attributifyToClass = unplugin.webpack as (options?: Options) => any
 
 // TODO: some upstream lib failed generate invalid dts, remove the any in the future
 export {
-  presetAttributifyWechat,
+  attributifyToClass,
   defaultAttributes,
   defaultIgnoreNonValuedAttributes,
 }
